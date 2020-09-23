a6b78fc6fa8e52213394bd3673d5197a3b16d4bd
villarrealtay <villarrealtay@gmail.com>
2020-09-23 18:39:05 -0300

    fin

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -8,7 +8,7 @@ const userActions = {
 			const response = await axios.post(RUTA_API+'/api/user/register', user)
 			console.log(response)
             if(response.data.success === "false") {
-                set({status: false})
+                //set({status: false})
                 let errors = response.data.error.errors;
 				if(errors.username !== undefined) toast.error(errors.username.message);
 				if(errors.mail !== undefined ) toast.error(errors.mail.message);
