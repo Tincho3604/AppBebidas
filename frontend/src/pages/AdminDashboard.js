c20a2c252a376f6f320c74482280329e7ab1a74c
AleLimia <nekro.lin@gmail.com>
2020-09-23 12:48:15 -0300

    Avances en dashboard

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import AdminHeader from '../components/AdminHeader';
 import NavAdmin from '../components/NavAdmin';
 import '../styles/adminDashboard.css'
 
 const AdminDashboard = (props) => {
 	return ( <>
-	<NavAdmin />
-	</> );
+	<AdminHeader />
+	<div className="dashboard">
+		<NavAdmin />
+		<div className="dataPanel">
+			<div className='adminData'>
+				<span>5</span>
+				<span>Ordenes pendientes</span>
+			</div>
+			<div className='adminData'>
+				<span>3</span>
+				<span>Articulos sin stock</span>
+			</div>
+			<div className='adminData'>
+				<span>35</span>
+				<span>Cantida de usuarios</span>
+			</div>
+			<div className='adminData'>
+				<span>7</span>
+				<span>Reseñas hechas</span>
+			</div>
+		</div>
+	</div></> );
 }
  
 export default AdminDashboard;
