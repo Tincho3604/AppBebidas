a4b2c50fb6ca7446b867c0502e659274a5f162e9
AleLimia <nekro.lin@gmail.com>
2020-09-24 12:06:59 -0300

    cambio de branch

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NavProducts from '../components/NavProducts';
 
 const Products = (props) => {
+	useEffect(() => {
+		// action que trae productos de una categoria
+		// props.get
+	}, [])
 	return ( <>
 	<NavProducts />
 	<div className='products'>
-		
+
 	</div>
 	</> );
 }
  
-export default Products;
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = {
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Products);
